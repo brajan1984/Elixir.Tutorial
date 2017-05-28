f93693b1666695bb0f548e2b19e664899c2baae4
Blazej Pietrzyk <blazej.pietrzyk@gmail.com>
2017-05-28 09:12:09 +0200

    Building umbrella

--- kv_umbrella/apps/kv/mix.exs
+++ kv_umbrella/apps/kv/mix.exs
@@ -4,6 +4,10 @@ defmodule KV.Mixfile do
   def project do
     [app: :kv,
      version: "0.1.0",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -31,6 +35,6 @@ defmodule KV.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:plug, "~> 1.0"}]
   end
 end

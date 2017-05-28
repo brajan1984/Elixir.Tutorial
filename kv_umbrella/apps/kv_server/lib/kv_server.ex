bb91026a787bc747bff3c5ef28412fc54498b8d3
Blazej Pietrzyk <blazej.pietrzyk@gmail.com>
2017-05-28 20:52:22 +0200

    After refacotring in KVServer.server

--- kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -29,18 +29,24 @@ defmodule KVServer do
 
   defp serve(socket) do
     msg = 
-      case read_line(socket) do
-        {:ok, data} ->
-          case KVServer.Command.parse(data) do
-            {:ok, command} -> 
-              KVServer.Command.run(command)
-            {:error, _} = err ->
-              err
-          end
-        {:error, _} = err ->
-              err
-      end
+      with 
+        {:ok, data} <- read_line(socket),
+        {:ok, command} <- KVServer.Command.parse(data),
+        do: KVServer.Command.run(command)
 
+      # Above code is equivalent to
+      # case read_line(socket) do
+      #   {:ok, data} ->
+      #     case KVServer.Command.parse(data) do
+      #       {:ok, command} -> 
+      #         KVServer.Command.run(command)
+      #       {:error, _} = err ->
+      #         err
+      #     end
+      #   {:error, _} = err ->
+      #         err
+      # end
+      
     write_line(socket, msg)
     serve(socket)
   end

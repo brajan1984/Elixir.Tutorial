91648bf0776fbefa91914e3fff499537abded738
Blazej Pietrzyk <blazej.pietrzyk@gmail.com>
2017-05-28 14:03:39 +0200

    Supervised echoserver

--- kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,46 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for KVServer.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    # The options below mean:
+    #
+    # 1. `:binary` - receives data as binaries (instead of lists)
+    # 2. `packet: :line` - receives data line by line
+    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
+    #
+    {:ok, socket} = :gen_tcp.listen(port,
+                      [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
 
-  ## Examples
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
 
-      iex> KVServer.hello
-      :world
+    #This makes the child process the “controlling process” of the client socket. 
+    #If we didn’t do this, the acceptor would bring down all the clients if it crashed
+    #because sockets would be tied to the process that accepted them (which is the default behaviour).
+    :ok = :gen_tcp.controlling_process(client, pid)
 
-  """
-  def hello do
-    :world
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
--- kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -12,6 +12,8 @@ defmodule KVServer.Application do
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg1, arg2, arg3)
       # worker(KVServer.Worker, [arg1, arg2, arg3]),
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [2121]])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
